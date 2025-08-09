ca008cc606609d40266fbf57bbbb8d328846b532
BS00761_Nguyễn Thị Ngọc Dung <ltndung1211@gmail.com>
2025-08-09 20:59:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import graphviz as st
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
